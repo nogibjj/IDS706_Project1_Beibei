da394e22bf16183a11011808357e24e83342c865
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-09-18 20:39:10 +0000

    add

--- querydb.py
+++ querydb.py
@@ -6,7 +6,7 @@ with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
                  access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
 
   with connection.cursor() as cursor:
-    cursor.execute("SELECT * FROM default.co2_emission_by_countries_csv LIMIT 2")
+    cursor.execute("SELECT * FROM default.day_2_csv LIMIT 2")
     result = cursor.fetchall()
 
     for row in result:
