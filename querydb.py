d3e48347a6c89f2352d93ad70abdb071ccc353b3
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-09-18 20:56:04 +0000

    ad

--- querydb.py
+++ querydb.py
@@ -1,13 +1,14 @@
-from databricks import sql
 import os
+from databricks import sql
 
 with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
                  http_path       = os.getenv("DATABRICKS_HTTP_PATH"),
                  access_token    = os.getenv("DATABRICKS_TOKEN")) as connection:
 
-  with connection.cursor() as cursor:
-    cursor.execute("SELECT * FROM default.day_2_csv LIMIT 2")
-    result = cursor.fetchall()
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM default.day_2_csv LIMIT 2")
+        result = cursor.fetchall()
 
     for row in result:
-      print(row)
+        print(row)
+    
