0aa9021d6a880761b90b6db9cfc651d73f346501
Beibei(Bella) Du <60382493+belladu0201@users.noreply.github.com>
2022-09-18 21:08:42 -0400

    add

--- querydb.py
+++ querydb.py
@@ -12,3 +12,18 @@ with sql.connect(server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME"),
     for row in result:
         print(row)
     
+def querydb(query="SELECT * FROM default.day_2_csv LIMIT 2"):
+    with sql.connect(
+        server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+        http_path=os.getenv("DATABRICKS_HTTP_PATH"),
+        access_token=os.getenv("DATABRICKS_TOKEN"),
+    ) as connection:
+
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+
+        for row in result:
+            print(row)
+
+    return result
